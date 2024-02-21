1e71284bc7ceb2b578defae6e718923764444ea5
markouny <a_markony@hotmail.com>
2024-02-20 16:10:50 -0800

    commit fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
-
-
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    """Type class MyList  with print_sorted function"""
 
     def print_sorted(self):
-        """prints the sorted list"""
         print(sorted(self))
