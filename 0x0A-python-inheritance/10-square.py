e0c05a1c41f5ea7fbaaed2b79f989ae48b2910f5
markouny <a_markony@hotmail.com>
2024-02-20 16:15:32 -0800

    commit 8 9 fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Type class of a square inherit a rectangle"""
+    """Represent a square"""
 
     def __init__(self, size):
-
+        """Initialize a new square
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+"""this module defines a Rectangle subclass Square"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Type class of a square inherit a rectangle"""
+    """Represent a square"""
 
     def __init__(self, size):
-
+        """Initialize a new square
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-
-        string = "[Square] " + str(self.__size) + "/" + str(self.__size)
-        return string
